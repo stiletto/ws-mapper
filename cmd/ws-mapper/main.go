985aee483411aac215562c1dab36a01ec5145fd4
Stiletto <blasux@blasux.ru>
2023-10-29 17:32:25 +0400

    Fix -config-template

--- cmd/ws-mapper/main.go
+++ cmd/ws-mapper/main.go
@@ -13,9 +13,11 @@ func main() {
 	config := wsmapper.DefaultConfig()
 	var configFile string
 	var example bool
+	var configTemplate bool
+	defaultConfig := "/etc/ws-mapper.yaml"
 	flag.BoolVar(&example, "example", false, "Print example config")
-	flag.StringVar(&configFile, "config", "/etc/ws-mapper.yaml", "Config file")
-	flag.BoolVar(&example, "config-template", false, "Print active config and exit")
+	flag.StringVar(&configFile, "config", defaultConfig, "Config file")
+	flag.BoolVar(&configTemplate, "config-template", false, "Print active config and exit")
 	flag.Parse()
 	if example {
 		config = wsmapper.ExampleConfig()
@@ -32,8 +34,17 @@ func main() {
 		}
 	} else {
 		slog.Error("Failed to open config file", "err", err, "config", configFile)
-		os.Exit(78)
+		if !configTemplate {
+			os.Exit(78)
+			return
+		}
+	}
+	if configTemplate {
+		yaml.NewEncoder(os.Stdout).Encode(config)
 		return
+	} else {
+		cfgBytes, _ := yaml.Marshal(config)
+		slog.Info("Active config:\n" + string(cfgBytes))
 	}
 	if err := wsmapper.CheckAndFixConfig(&config); err != nil {
 		slog.Error("Configuration error", "err", err, "config", configFile)
